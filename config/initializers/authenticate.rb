5722777ed6e8b757abc785661d70392af308d701
Andy <dodgerogers@hotmail.com>
2013-12-05 11:06:20 -0500

    defer js, authenticate refactor

--- config/initializers/authenticate.rb
+++ config/initializers/authenticate.rb
@@ -1,12 +1,15 @@
 ActionController::HttpAuthentication::Digest.module_eval do
-  def authentication_request_with_customization(controller, realm, message = nil)
+  def authentication_request(controller, realm, message = nil)
     message = controller.render_to_string(template: "errors/401")
-    authentication_request_without_customization(controller, realm, message)
+    authentication_header(controller, realm)
+    controller.response_body = message
+    controller.status = 401
   end
-  alias_method_chain :authentication_request, :customization
 end
-
-# controller.response_body = controller.redirect_to EstherRaday::Application.routes.url_helpers.root_path
-# controller.flash[:error] = message
-# controller.render ActionView::Base.new("app/views/", {}, ActionController::Base.new).render(template: "errors/401", layout: "layouts/application")
-# controller.response_body = ActionView::Base.new("app/views/", {}, ActionController::Base.new).render(template: "errors/401", layout: "layouts/application")
+    
+# def authentication_request_with_customization(controller, realm, message = nil)
+#     message = controller.render_to_string(template: "errors/401")
+#     authentication_request_without_customization(controller, realm, message)
+#   end
+#   alias_method_chain :authentication_request, :customization
+# end    
