c105c67fdb8cf5a2af627531cd86b0c22aa4b08b
Andy <dodgerogers@hotmail.com>
2013-12-04 23:20:52 -0500

    module_eval http digest authentication

--- config/initializers/authenticate.rb
+++ config/initializers/authenticate.rb
@@ -1,13 +1,9 @@
-module ActionController
-  module HttpAuthentication
-    module Digest
-      def authentication_request_with_customization(controller, realm, message = nil)
-        message = controller.render_to_string(template: "errors/401")
-        authentication_request_without_customization(controller, realm, message)
-      end
-      alias_method_chain :authentication_request, :customization
-    end
+ActionController::HttpAuthentication::Digest.module_eval do
+  def authentication_request_with_customization(controller, realm, message = nil)
+    message = controller.render_to_string(template: "errors/401")
+    authentication_request_without_customization(controller, realm, message)
   end
+  alias_method_chain :authentication_request, :customization
 end
 
 # controller.response_body = controller.redirect_to EstherRaday::Application.routes.url_helpers.root_path
