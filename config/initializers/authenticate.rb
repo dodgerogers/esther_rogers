3995b9584be0e4667912b848bffed90d00a645cb
Andy <dodgerogers@hotmail.com>
2013-12-04 22:52:37 -0500

    http monkey patch, redirects to custom 401

--- config/initializers/authenticate.rb
+++ config/initializers/authenticate.rb
@@ -1,15 +1,16 @@
-# include EstherRaday::Application.routes.url_helpers
-# ActionController::HttpAuthentication::Digest.module_eval do
-#   def authentication_request(controller, realm, message = nil)
-#     message ||= "You are unauthorized to access this page.\n"
-#     authentication_header(controller, realm)
-#     controller.response_body = controller.redirect_to EstherRaday::Application.routes.url_helpers.root_path
-#     controller.flash[:error] = message
-#     controller.status = 401
-#   end
-# end
-# 
-#controller.response_body = controller.redirect_to EstherRaday::Application.routes.url_helpers.root_path
-#controller.flash[:error] = message
-#controller.render ActionView::Base.new("app/views/", {}, ActionController::Base.new).render(template: "errors/401", layout: "layouts/application")
-#controller.response_body = ActionView::Base.new("app/views/", {}, ActionController::Base.new).render(template: "errors/401", layout: "layouts/application")
+module ActionController
+  module HttpAuthentication
+    module Digest
+      def authentication_request_with_customization(controller, realm, message = nil)
+        message = controller.render_to_string(template: "errors/401")
+        authentication_request_without_customization(controller, realm, message)
+      end
+      alias_method_chain :authentication_request, :customization
+    end
+  end
+end
+
+# controller.response_body = controller.redirect_to EstherRaday::Application.routes.url_helpers.root_path
+# controller.flash[:error] = message
+# controller.render ActionView::Base.new("app/views/", {}, ActionController::Base.new).render(template: "errors/401", layout: "layouts/application")
+# controller.response_body = ActionView::Base.new("app/views/", {}, ActionController::Base.new).render(template: "errors/401", layout: "layouts/application")
